3f9eac0b99f44763aa5c85402bedfe3ec58fbf4b
13orangminus <luqmanmaliki18@gmail.com>
2025-11-02 21:45:48 +0700

    Refactor HTML and JavaScript files; add new terminal interface and functionality

--- prt1/script.js
+++ prt1/script.js
@@ -12,7 +12,19 @@ function sayHello() {
     console.timeEnd("f1");
 }
 
-// NB From Me :  
+// NB From Me :
+//function sayHello() adalah sebuah fungsi JavaScript yang melakukan beberapa tindakan ketika dipanggil. Berikut adalah penjelasan dari setiap bagian kode di dalam fungsi tersebut:
+// console.count() digunakan untuk menghitung berapa kali sebuah kode dieksekusi.
+// console.countReset() digunakan untuk mereset hitungan dari console.count().
+// console.time() digunakan untuk memulai timer dengan label tertentu.
+// console.timeEnd() digunakan untuk menghentikan timer dan mencetak waktu yang telah berlalu sejak timer dimulai.
+// console.timeLog() digunakan untuk mencatat waktu yang telah berlalu tanpa menghentikan timer.
+// console.group() digunakan untuk mengelompokkan beberapa log bersama-sama dalam konsol.
+// console.groupEnd() digunakan untuk mengakhiri pengelompokan log yang dimulai dengan console.group().
+// console.table() digunakan untuk menampilkan data dalam format tabel di konsol.
+// console.warn() digunakan untuk menampilkan pesan peringatan di konsol.
+// console.error() digunakan untuk menampilkan pesan kesalahan di konsol.   
+//console.info() digunakan untuk menampilkan pesan informasi di konsol.
 
 function resetCount() {
     console.countReset("submited");
@@ -40,5 +52,4 @@ console.log('Payload Size:', "Successfull");
 console.groupEnd();
 
 // console.info("Test Log Info");
-
 
